8dcdec6bba6cc82fc9372b3311074ffde1f68e03
Samuel Mulkens <sammulkens@gmail.com>
2018-09-05 15:04:48 +0200

    Made a working json file reader module addition

--- app.js
+++ app.js
@@ -2,7 +2,14 @@
 var app = express();
 var fs = require('fs')
 var bodyParser = require('body-parser')
-let users = require('./users.json') 
+const jsonReader = require('./json-file-reader.js')
+let users = jsonReader("users.json", callback);
+
+function callback(obj) {
+	users = obj;
+}
+
+// let users = jsonReader('./users.json');
 
 //used to load the css files in public
 app.use(express.static('public'));
@@ -17,17 +24,10 @@ app.get("/", function(req, res) {
 });
 
 //render users page and load the JSON file. Have to include the .render in scop of readfile function to ensure updating after changes in adduser.  
-app.get("/users", function(req, res) {
-	fs.readFile('users.json', function (err, data) {
-		if (err) {
-			throw err;
-		}
+app.get("/users", function(req, res) { 
 
-    users = JSON.parse(data)
     res.render("users", {users: users});
 	})
-	
-})
 
 //render search page
 app.get("/search", function(req, res) {
@@ -49,27 +49,16 @@ app.get("/adduser", function(req, res) {
 
 //adding a new user to the JSON file. 
 app.post("/adduser", function(req, res) {	
-	var newdata = req.body
+	var newdata = req.body;
+    users.push(newdata)
 
-	fs.readFile('users.json', function (err, data) {
-		if (err) {
-			throw err;
-		}
-
-    var json = JSON.parse(data)
-    json.push(newdata)
-
-    fs.writeFile("users.json", JSON.stringify(json), function(err, data) { 
+    fs.writeFile("users.json", JSON.stringify(users), function(err, data) { 
     	if (err) {
     		throw err;
     	}
-
+    	res.redirect("users")
 		})
 	})
-
-	//redirect to home page
-	res.redirect("users")
-})
 
 app.listen(3000, function() {
 	console.log("The server is running on port 3000");
